755386d4e827858a40be2934fa937801cd9252b9
Alexander <alexanderholmberg91@gmail.com>
2021-11-16 09:37:52 +0100

    checks the validity of instructions correctly

--- interpreter/hello.go
+++ interpreter/hello.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -10,9 +11,9 @@ import (
 var registers = [4]int{0, 0, 0, 0}
 
 // 3 arrays containing the different types of instructions we allow
-var rType = [4]string{"add", "sub", "set", "jeq"}
-var jType = [1]string{"j"}
-var specType = [3]string{"input", "print", "exit"}
+var registerType = []string{"add", "sub", "set", "jeq"}
+var jumpType = []string{"j"}
+var specialType = []string{"input", "print", "exit"}
 
 func readLineForLine(file string) []string {
 	data, err := os.ReadFile(file)
@@ -44,8 +45,86 @@ func readLineForLine(file string) []string {
 	return instructions
 }
 
+// Helper function that checks if a slice contains a string
+func stringInSlice(a string, list []string) bool {
+	for _, b := range list {
+		if a == b {
+			return true
+		}
+	}
+	return false
+}
+
+func instructionsAreValid(instructions []string) bool {
+	for i, line := range instructions {
+		//fmt.Println(i, line)
+		words := strings.Split(line, " ")
+		if stringInSlice(words[0], registerType) {
+			fmt.Println(words[0])
+			// if it's a registerType, length of the whole instructions must be 4
+			if len(words) != 4 {
+				fmt.Printf("Error on line %d: Incorrect instruction of length %d\n", i+1, len(words))
+				return false
+			} else {
+
+				var j int
+				for j = 1; j < 3; j++ {
+					fmt.Println(strings.Index(string(words[j][0]), "#"))
+					// Register must start with '#'
+					if !strings.Contains(string(words[j][0]), "#") {
+						fmt.Printf("Error on line %d: Missing '#'\n", j+1)
+						return false
+					}
+				}
+				// first arg to an registerType must start with #1, #2 or #3
+				if !stringInSlice(string(words[1][1]), []string{"1", "2", "3"}) {
+					fmt.Printf("Error on line %d: %s cannot be used as first register argument for r_type instructions\n", i+1, string(words[1][1]))
+					return false
+				}
+
+				// immidiate can only be a 1 bit value
+				if !stringInSlice(string(words[3]), []string{"0", "1"}) {
+					fmt.Printf("Error on line %d: invalid immidiate. Can only be a 1 bit value\n", i+1)
+					return false
+				}
+
+			}
+		} else if stringInSlice(words[0], jumpType) {
+			// jump instruction should only have a total length of 2
+			if len(words) != 2 {
+				fmt.Printf("Error on line %d: Incorrect instruction of length %d\n", i+1, len(words))
+				return false
+			}
+
+			wordAsInt, err := strconv.Atoi(words[1])
+			if err != nil {
+				panic(err)
+			}
+
+			// 5 bit signed value range is (-16, 15)
+			if wordAsInt > 15 || wordAsInt < -16 {
+				fmt.Printf("Error on line %d: %d does not fit in a 5 bit (signed) value\n", i+1, wordAsInt)
+				return false
+			}
+
+		} else if stringInSlice(words[0], specialType) {
+			// Special instructions should only have length 1
+			if len(words) != 1 {
+				fmt.Printf("Error on line %d: Incorrect instruction of length {%d}\n", i+1, len(words))
+				return false
+			}
+		} else {
+			// "" means it's just an empty line
+			if words[0] != "" {
+				fmt.Printf("Error on line %d: unknown command: {%s}\n", i+1, words[0])
+				return false
+			}
+		}
+	}
+	return true
+}
+
 func main() {
-	//fmt.Println(j_type)
 	// read from stdin and print the output
 	fmt.Println(len(os.Args))
 	if len(os.Args) != 2 {
@@ -53,6 +132,7 @@ func main() {
 	} else {
 		file := os.Args[1]
 		fmt.Println(file)
+		// check if file exists
 		// check that last 5 chars is .bbvv
 		if len(file) < 5 || file[len(file)-5:] != ".bbvv" {
 			fmt.Println("bs file =", file)
@@ -60,6 +140,11 @@ func main() {
 			// it's a legit bbvv file
 			instructions := readLineForLine(file)
 			fmt.Println(instructions)
+			if instructionsAreValid(instructions) {
+
+			} else {
+				fmt.Println("Provided instructions are not valid.")
+			}
 		}
 	}
 	//_ := os.Args[0]
